a0eb24acbb710eabb5306d3c38f1cc692f86a534
Khoa Le <s4khleee@uni-trier.de>
2024-05-06 12:21:34 +0000

    use TARGET env var in build script

--- build_demo.sh
+++ build_demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -e 's/demo\.\([^\.]\)*/demo/g' demo/dist/index.html > ../src/cmd/server/index.html
-rm ../src/cmd/server/demo.* 
-cp demo/dist/demo.*.js ../src/cmd/server/demo.js
-cp demo/dist/demo.*.css ../src/cmd/server/demo.css
+sed -e 's/demo\.\([^\.]\)*/demo/g' demo/dist/index.html > $1/cmd/server/index.html
+rm $1/cmd/server/demo.* 
+cp demo/dist/demo.*.js $1/cmd/server/demo.js
+cp demo/dist/demo.*.css $1/cmd/server/demo.css
